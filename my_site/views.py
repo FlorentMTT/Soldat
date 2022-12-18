ac9f2717dfc78986ed2097a9e27a2e6d80e4b8f4
FlorentMTT <florent.mttt@eduge.ch>
2022-12-18 22:18:06 +0100

    maj form

--- my_site/views.py
+++ my_site/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from my_site.models import *
 from templates.forms import *
+
 # Create your views here.
 def base(request):
     return render(request, "base.html")
@@ -11,6 +12,18 @@ def voir_soldat(request):
     return render(request, "voir_soldat.html", contenu)
 
 def creer_soldat(request):
-    form = FormAjouterSoldat()
-    contenu = {"formulaire" : form}
-    return render(request, "creer_soldat.html", contenu)
+    if  request == "POST":
+        form = FormAjouterSoldat(request.POST)
+        if form.is_valid():
+            nouveau_soldat = Soldat()
+            nouveau_soldat.nom = form.cleaned_data['nom']
+            nouveau_soldat.grade = form.cleaned_data['grade']
+            nouveau_soldat.specialite = form.cleaned_data['specialite']
+            nouveau_soldat.save()
+            return redirect('voir_soldat')
+        else:
+            print("Erreur, formulaire invalide")
+    else:
+        form = FormAjouterSoldat()
+        contenu = {"formulaire" : form}
+        return render(request, "creer_soldat.html", contenu)
